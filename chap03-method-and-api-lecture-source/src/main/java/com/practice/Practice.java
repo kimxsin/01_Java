12f720f3a61bffc81f53b0854e8039ac164f8b5c
DESKTOP-ET0NPMS\hi <rhlanf9063@gmail.com>
2024-08-28 17:09:15 +0900

    [0828]

---
+++ chap03-method-and-api-lecture-source/src/main/java/com/practice/Practice.java
@@ -0,0 +1,31 @@
+package com.practice;
+
+public class Practice {
+    public static void main(String[] args) {
+
+    Practice prt1 = new Practice();
+    prt1.methodA();
+    prt1.methodB();
+    prt1.methodC();
+    prt1.methodD();
+
+    }
+
+    public void methodA() {
+        System.out.print("은미님 ");
+        System.out.println("안녕하세요?");
+
+    }
+    public void methodB() {
+        System.out.print("정현님 ");
+        System.out.println("안녕하세요?");
+    }
+    public void methodC() {
+        System.out.print("승현님 ");
+        System.out.println("안녕하세요?");
+    }
+    public void methodD() {
+        System.out.print("이정님 ");
+        System.out.println("안녕하세요?");
+    }
+}
